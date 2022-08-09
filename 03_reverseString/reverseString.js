ec0bbf5a801ad518967cca891b45889db467442a
xmusil2 <xmusil2@mendelu.cz>
2022-08-09 21:50:18 +0200

    1.2.3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let length = string.length;
+    let newString = "";
+    while (length >= 0){
+        newString += string.charAt(length);
+        length--;
+    }
+    return(newString);
 };
 
 // Do not edit below this line
