d981a47b676a3e6856078adbf268ea98ff231c93
xmusil2 <xmusil2@mendelu.cz>
2022-08-09 22:16:43 +0200

    4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
 
+    var arr = arguments[0];
+    for (var j = 1; j < arguments.length; j++){
+        for( var i = 0; i < arr.length; i++){  
+            if ( arr[i] === arguments[j]) {
+                arr.splice(i, 1); 
+            }  
+        }
+    }   
+    return(arr)
 };
 
 // Do not edit below this line
