001283844a1d4384bdaaa20daec336e327780ebc
xmusil2 <xmusil2@mendelu.cz>
2022-08-09 23:11:42 +0200

    7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
+const ftoc = function(f) {
+  let c = ((f - 32) * 5) /9;
+  return(Math.round(c*10)/10);
 
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = 1.8 * c + 32;
+  return(Math.round(f*10)/10);
 };
 
 // Do not edit below this line
